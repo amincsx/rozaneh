8c36163a2a36a3eca8d73358fd3f483cbe61295d
amin <amincsx@gmail.com>
2025-12-15 13:28:39 -0600

    chore(types): add declaration for jalaali-js

---
+++ src/types/jalaali-js.d.ts
@@ -0,0 +1,16 @@
+declare module 'jalaali-js' {
+  export function toJalaali(gy: number, gm: number, gd: number): {
+    jy: number;
+    jm: number;
+    jd: number;
+  };
+
+  export function toGregorian(jy: number, jm: number, jd: number): {
+    gy: number;
+    gm: number;
+    gd: number;
+  };
+
+  export function isLeapJalaali(jy: number): boolean;
+  export function isLeapGregorian(gy: number): boolean;
+}
